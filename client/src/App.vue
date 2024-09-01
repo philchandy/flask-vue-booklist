1a5c8a343c2d812df4eb2e1ddb37e8977af9d752
Phil Chandy <phillipchandy200@gmail.com>
2024-09-01 15:14:14 -0400

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<style>
+#app {
+  margin-top: 60px
+}
+</style>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+
